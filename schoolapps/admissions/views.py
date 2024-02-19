4bc23eb6c58c4073e96ad0f3298e3d4932cc6878
archanaeerla <eerlarchana@gmail.com>
2024-02-19 22:20:18 +0530

    created admisssons 1 and 2

--- schoolapps/admissions/views.py
+++ schoolapps/admissions/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.urls import path
+from django.http import HttpResponse
 
 # Create your views here.
+def admission1(request):
+    return HttpResponse("this is my first admission page")
+
+def admissions2(request):
+    return HttpResponse("this is my second admisssion page")
