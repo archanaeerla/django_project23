1a933117c96bca640c72a238272e9be46b39f971
archanaeerla <eerlarchana@gmail.com>
2024-02-22 20:11:14 +0530

    added urls in financial

--- schoolapps/admissions/urls.py
+++ schoolapps/admissions/urls.py
@@ -3,5 +3,5 @@ from admissions import views
 
 urlpatterns=[
     path('createadmission/',views.admissions1),
-    path('viewadmission/',views.admissions2)
+    path('viewadmission/',views.admissions2),
 ]
--- schoolapps/financial/urls.py
+++ schoolapps/financial/urls.py
@@ -1,1 +1,9 @@
 from django.urls import path
+from admissions.views import finance1
+from admissions.views import finance2
+
+
+urlpatterns=[
+    path('paybill/',finance1),
+    path('viewbill/',finance2),
+]
