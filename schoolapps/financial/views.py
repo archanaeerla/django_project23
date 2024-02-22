28cc6b99e47c1eaaef3b4ef91ce4a9b0f07664d3
archanaeerla <eerlarchana@gmail.com>
2024-02-22 18:51:21 +0530

    changes made by me

--- schoolapps/financial/views.py
+++ schoolapps/financial/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def finance1(request):
+    return HttpResponse("this is my first financial page")
+
+def finance2(request):
+    return HttpResponse("this is my second financial page")
